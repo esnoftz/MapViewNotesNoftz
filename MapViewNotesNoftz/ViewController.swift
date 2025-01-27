9bbb529d0ac40cbf07130753d0102cf7c0176ee2
esnoftz <esnoftz@gmail.com>
2025-01-27 08:49:23 -0600

    Worked on notes.

--- MapViewNotesNoftz/ViewController.swift
+++ MapViewNotesNoftz/ViewController.swift
@@ -9,14 +9,27 @@ import UIKit
 // import the map kit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapViewOutlet: MKMapView!
     
     var locationManager = CLLocationManager()
     
+    var currentLocation: CLLocation!
+    
+    var parks: [MKMapItem] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // certain methods in delegate auto called
+        locationManager.delegate = self
+        
+        // updates your location as it changes
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        
         // go to info to see first step
         
         // hold down option key and click on showsUserLocation to see description of what it does
@@ -33,7 +46,46 @@ class ViewController: UIViewController {
         mapViewOutlet.setRegion(region2, animated: true)
         
     }
+    
+    
+    // automatically called when your location is updated
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // most recent location is at index 0
+        currentLocation = locations[0]
+    }
 
+    
+    @IBAction func searchAction(_ sender: UIButton) {
+        // blank request object
+        let request = MKLocalSearch.Request()
+        
+        request.naturalLanguageQuery = "Parks"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        
+        let search = MKLocalSearch(request: request)
+        
+        // says that when everything else is done, do this:
+        // response and error are parameters of this function we are building
+        search.start { (response, error) in
+            
+            // similar to if let (response cannot be nil -- does else if nil)
+            guard let response = response
+            else {return}
+            for mapItem in response.mapItems {
+                
+                // must use self when in closure trying to access a global variable
+                self.parks.append(mapItem)
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                self.mapViewOutlet.addAnnotation(annotation)
+                
+            }
+        }
+    }
+    
 
 }
 
